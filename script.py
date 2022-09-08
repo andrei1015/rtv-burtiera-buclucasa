02b62def99f86ed7bc388017f7ff8473ffa3cf45
Andrei <andrei1015@gmail.com>
2022-09-08 14:28:49 +0200

    first working code with file writing

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-# import cv2
+import cv2
 from wand.image import Image
 from wand.display import display
 from pytesseract import pytesseract
@@ -13,12 +13,19 @@ screensExist = os.path.exists(screens)
 cropsExist = os.path.exists(crops)
 
 # REFRESH DIRS DELETE OR SOMETHING
+if screensExist:
+  shutil.rmtree(screens, ignore_errors=False, onerror=None)
+  os.remove('burtiere.txt')
 
-if not screensExist:
-  os.makedirs(screens)
+
+path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.tesseract_cmd = path_to_tesseract
 
 
-os.system("ffmpeg -headers \"referer: https://www.romaniatv.net/\" -i \"https://livestream.romaniatv.net/clients/romaniatv/playlist.m3u8\" -vf fps=1/5 -t 00:01:00 screens/output%05d.jpg")
+os.makedirs(screens)
+
+
+os.system("ffmpeg -loglevel quiet -headers \"referer: https://www.romaniatv.net/\" -i \"https://livestream.romaniatv.net/clients/romaniatv/playlist.m3u8\" -vf fps=1/5 -t 00:05:00 screens/output%06d.jpg")
 
 for filename in os.listdir(screens):
 
@@ -31,17 +38,35 @@ for filename in os.listdir(screens):
         # save resized image
         img.save(filename = 'screens/cropped_' + filename)
 
-if not cropsExist:
-  os.makedirs(crops)
+os.makedirs(crops)
 
-for src_file in Path(screens).glob('cropped_*.jpg'):
+for src_file in Path(screens).glob('cropped_*.*'):
     shutil.copy(src_file, crops)
+    os.remove(src_file)
 
-# path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-# pytesseract.tesseract_cmd = path_to_tesseract
+for imageName in os.listdir (crops):
+  inputPath = os.path.join(crops, imageName)
+  img = cv2.imread(inputPath)
+  text = pytesseract.image_to_string(img, lang='ron')
 
-# # img = cv2.imread("test2.jpg")
+  print(text)
+  
+  with open('burtiere.txt', 'a', encoding='utf-8') as f:
+    f.write(text + ' : ' + imageName + '\n\n')
 
-# text = pytesseract.image_to_string("screens/output00000021.jpg", lang='ron')
-# # im1.show()
-# print(text)
+# for image in os.listdir(crops):
+#         print(os.path.join(crops, image))
+#         inputPath = os.path.join(crops, image)
+#         cropped_file = open(inputPath)
+#         text = pytesseract.image_to_string(cropped_file, lang='ron')
+
+#         with open('burtiere.txt', 'w') as f:
+#           f.write(text + ' : ' + cropped_file)
+
+# for cropped_file in Path(crops).glob('*.*'):
+#   #img = cv2.imread("test2.jpg")
+#   print(cropped_file)
+#   text = pytesseract.image_to_string(cropped_file, lang='ron')
+
+#   with open('burtiere.txt', 'w') as f:
+#     f.write(text + ' : ' + cropped_file)
